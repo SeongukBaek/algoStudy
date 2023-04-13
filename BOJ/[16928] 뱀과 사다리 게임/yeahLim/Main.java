92e013b777665db142eec2306e40762f7f98875e
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2023-04-13 16:03:38 +0900

    10week : BOJ[16928] 뱀과 사다리 게임 UPDATE CODE

--- BOJ/[16928] 뱀과 사다리 게임/yeahLim/Main.java
+++ BOJ/[16928] 뱀과 사다리 게임/yeahLim/Main.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int n, m;
 	static int[] board = new int[101];
 	
 	public static void main(String[] args) throws IOException {
@@ -10,8 +9,8 @@ public class Main {
 		/* 입력 및 변수 초기화 */
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		for(int i=0; i<n; i++) {
 			st = new StringTokenizer(br.readLine());
 			board[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
