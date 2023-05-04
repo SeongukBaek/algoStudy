cc6af01bf4a1de66c573137fd46a0bf255374119
kjg_0701@naver.com <kjg070146@gmail.com>
2023-05-04 10:31:20 +0900

    13week : Programmers[92341] 주차 요금 계산 update

--- Programmers/[92341] 주차 요금 계산/jungu12/Solution.java
+++ Programmers/[92341] 주차 요금 계산/jungu12/Solution.java
@@ -2,25 +2,28 @@ import java.util.*;
 class Solution {  
     public int[] solution(int[] fees, String[] records) {
         Map<String, Integer> carInfo = new HashMap<>();
-        List<Integer> answerList = new ArrayList<>();
         
         for(int i = 0; i < records.length; i++) {
             String[] recordIn = records[i].split(" ");
             String[] strInTime = recordIn[0].split(":");
+            
+            //입차 정보 찾아서 저장
             if(recordIn[2].equals("IN")) {
                 int inTime = Integer.parseInt(strInTime[0]) * 60 + Integer.parseInt(strInTime[1]);
                 //출차 기록이 없다면 24:00(1439) 저장됨
                 int outTime = 1439;
                 
+                //출차 정보 찾기
                 for(int j = i + 1; j < records.length; j++) {
                     String[] recordOut = records[j].split(" ");
-                    //입차된 차의 출차 정보를 찾음
+                    //입차된 차의 출차 정보 찾아서 저장
                     if(recordOut[1].equals(recordIn[1])) {
                         String[] strOutTime = recordOut[0].split(":");
                         outTime = Integer.parseInt(strOutTime[0]) * 60 + Integer.parseInt(strOutTime[1]);
                         break;
                     }
                 }
+                
                 //여러번 입차한 경우
                 if(carInfo.containsKey(recordIn[1])) {
                     carInfo.put(recordIn[1], carInfo.get(recordIn[1]) + (outTime - inTime));
@@ -43,12 +46,15 @@ class Solution {
             int parkingTime = carInfo.get(carNum);
             System.out.println(carNum + " : " + parkingTime);
             
+            //주차 시간이 기본 시간 미만이라면 기본요금 부여
             if(parkingTime < fees[0]) {
                 answer[idx] = fees[1];
+                idx++;
                 continue;
             }
-
-            answer[idx] = fees[1] + (int)Math.ceil((parkingTime - fees[0]) / (double)fees[2]) * fees[3];
+            
+            //주차 시간이 기본 시간 이상이라면 주어진 공식에 따라 기본요금 부여
+            answer[idx] = fees[1] + (int)Math.ceil(((double)parkingTime - fees[0]) / fees[2]) * fees[3];
             idx++;
         }
         
