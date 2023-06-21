23dd2c29381e86a85fb934eee8ace6dc3d32b987
kjg_0701@naver.com <kjg070146@gmail.com>
2023-06-21 17:16:00 +0900

    17week : Programmers[42584] 주식가격

---
+++ Programmers/[42584] 주식가격/jungu12/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j]) 
+                    break;
+
+            }
+        }
+
+        return answer;
+    }
+}
